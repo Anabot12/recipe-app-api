92b8270f5300608540826b51c7112f8f6b84257c
AnanyaM12 <ananyaamathur03@gmail.com>
2023-06-14 13:56:50 +0530

    new

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,8 +5,9 @@ from django.test import SimpleTestCase
 
 from app import calc
 
-class CalcTests(SimpleTestCase):
-    def test_add_numbers(self):
+
+class CalcTests (SimpleTestCase):
+    def test_add_numbers (self) :
         res=calc.add(5 , 6)
         self.assertEqual(res , 11)
     
